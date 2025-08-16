9d3f656a707e2bafda03a5e67252c1bc257439c7
yanellaft <yanellaft@gmail.com>
2025-08-15 21:14:57 -0400

    made base

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Choose a category to quiz yourself in:")
-categories = (("Math"), ("Science"), ("History"), ("Literature"))
-category = input(categories)
+#roman to number conversion
+def romanToNumber(s):
+    numerals = ['I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000]
+    print('Enter a Roman numeral:')
+    s = s.upper()
+#adding numbers together
+
+#number to roman conversion
