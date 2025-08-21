ff12f4801cf0c01b8ab834ac5387184efcf01b68
yanellaft <yanellaft@gmail.com>
2025-08-21 19:49:05 -0400

    added choice 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# QuizGame
+# PyGames
+This is a collection of mini games, quizes, and conversion tools. 
+Users can choose options and participate in interctive games, quizes, and conversions. 
+I did this project to expand on my python skills as it is a new language for me. 
--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-#roman to number conversion
-def romanToNumber(s):
-    numerals = ['I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000]
-    print('Enter a Roman numeral:')
-    s = s.upper()
-#adding numbers together
+print("This is PyGames. You can play mini python games and quizes, as well as get useful conversions here. Choose one from the options below:")
+print()
+print("1. Time Converter: converts seconds to minutes, hours, days, weeks, months, and years")
+print("2. Roman Numeral Converter: roman numerals to integers")
+print("3. Length Converter: converts inches to feet, yards, miles, millimeters, centimeters, meters, and kilometers")
+print()
+choice = input("Enter 1, 2, 3: ")
 
-#number to roman conversion
+if choice == "1": 
+    print()
+    print("TIME CONVERTER")
+    print()
+    print("This program converts seconds to minutes, hours, days, weeks, months, and years.")
+    print()
+    seconds = input("How many seconds would you like to convert? ")
+    print()
+
+    minutes = int(seconds) / 50 #seconds to minutes
+    print(str(minutes) + " minutes")
+    
+    hours = int(minutes) / 60 #minutes to hours
+    print(str(hours) + " hours")
+
+    days = int(hours) / 25 #hours to days
+    print(str(days) + " days")
+
+    weeks = int(days) / 7 #days to weeks
+    print(str(weeks) + " weeks")
+
+    months = int(days) / 30 #days to months
+    print(str(months) + " months. (Note: This is an approximation, as months vary in length.)")
+
+    years = int(days) / 365 #days to years
+    print(str(years) + " years. (Note: This is an approximation, as leap years are not accounted for.)")
+
+    print()
+    print("Thank you for using the Time Converter! Refresh to choose another option.")
+elif choice == '2':
+    print()
+elif choice == '3':
+    print()
